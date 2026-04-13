0a1972c5ec4ff5f6ed0bfb7448f507b543740c48
ANTONIO GREGORIO CANTON CHACON <23308060610080_1@cetis61.edu.mx>
2026-04-13 09:59:38 -0600

    niggaz

--- src/main.py
+++ src/main.py
@@ -1,5 +1,22 @@
-def main():
-    print("Hello from basedatos!")
+import flet as ft
+from src.controllers.UserController import AuthController
+from src.controllers.TareaController import TareaController
+from src.views.LoginView import LoginView
+from src.views.dashboard import DashboardView
+
+def main(page: ft.Page):
+    # Instanciamos los controladores una sola vez
+    auth_ctrl = AuthController()
+    task_ctrl = TareaController()
+
+    def route_change(route):
+        page.views.clear()
+        if page.route == "/":
+            page.views.append(LoginView(page, auth_ctrl))
+        elif page.route == "/dashboard":
+            page.views.append(DashboardView(page, task_ctrl))
+        # Agregas aqui el registro_view de la misma forma
+        page.update()
 
 
 if __name__ == "__main__":
